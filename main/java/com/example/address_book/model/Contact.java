ad48d5d24d28a7431fb5876a7d53daccf4e89e59
Ashu <shabsingh961@gmail.com>
2025-04-21 19:01:43 +0530

    [ASHU]Add. UC3-> created Entity to make structure for table in databse

---
+++ main/java/com/example/address_book/model/Contact.java
@@ -0,0 +1,46 @@
+package com.example.address_book.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="contacts")
+public class Contact {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String phone;
+	private String email;
+	private String address;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
