f587c836e2438b41fd9b39fc3a0e63a156bcf2c7
Ashu <shabsingh961@gmail.com>
2025-04-22 22:38:03 +0530

    [ASHU]Add. Lombok lib for auto generated get and set method

--- main/java/com/example/address_book/dto/Response.java
+++ main/java/com/example/address_book/dto/Response.java
@@ -2,44 +2,61 @@ package com.example.address_book.dto;
 
 import java.time.LocalTime;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Response {
 	
+	@NotEmpty(message="status is mandatory")
 	private String status;
-	private String message;
-	private Object data;
-	private LocalTime time;
-	public Response(String string, String string2,Object data, LocalTime now) {
-		// TODO Auto-generated constructor stub
-		this.setStatus(string);
-		this.setMessage(string2);
-		this.setTime(now);
-		this.setData(data);
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public LocalTime getTime() {
-		return time;
-	}
-	public void setTime(LocalTime time) {
-		this.time = time;
-	}
-	public Object getData() {
-		return data;
-	}
-	public void setData(Object data) {
-		this.data = data;
-	}
 	
+	@NotBlank(message = "Message is mandatory")
+	private String message;
+	
+	@NotNull(message = "Data must not be null")
+	private Object data;
+	
+	@NotNull(message = "Time is required")
+	private LocalTime time;
+	
+//	public Response(String string, String string2,Object data, LocalTime now) {
+//		// TODO Auto-generated constructor stub
+//		this.setStatus(string);
+//		this.setMessage(string2);
+//		this.setTime(now);
+//		this.setData(data);
+//	}
+//	public String getMessage() {
+//		return message;
+//	}
+//	public void setMessage(String message) {
+//		this.message = message;
+//	}
+//	public String getStatus() {
+//		return status;
+//	}
+//	public void setStatus(String status) {
+//		this.status = status;
+//	}
+//	public LocalTime getTime() {
+//		return time;
+//	}
+//	public void setTime(LocalTime time) {
+//		this.time = time;
+//	}
+//	public Object getData() {
+//		return data;
+//	}
+//	public void setData(Object data) {
+//		this.data = data;
+//	}
+//	
 }
